7dacc018730a42be093227b6852a56f8f97569af
anuragc2001 <chakraborty.anurag01@gmail.com>
2023-02-19 16:05:55 +0530

    added auth to admin panel

--- backend/src/controllers/admin/admin.controller.js
+++ backend/src/controllers/admin/admin.controller.js
@@ -1,5 +1,9 @@
+const Admin = require('../../models/admin.model')
+
 const getAdmin = (req, res) => {
-    res.render('admin/adminPanel')
+    console.log(req.User);
+    const adminName = req.User.adminName
+    res.render('admin/adminPanel', {adminName})
 }
 
 module.exports = {
