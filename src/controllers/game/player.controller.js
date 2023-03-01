ed068b28f8af9f89b026cb838b7e628e9cadf4a2
anuragc2001 <chakraborty.anurag01@gmail.com>
2023-03-01 20:50:05 +0530

    added player schema and score check

--- src/controllers/game/player.controller.js
+++ src/controllers/game/player.controller.js
@@ -1,4 +1,5 @@
-const Score = require('../../models/player.model')
+const Score = require('../../models/player.model');
+const Player = require('../../models/register.dev.model');
 
 const getScore = (req, res) => {
     Score.find({}).sort({level: 1})
@@ -12,6 +13,26 @@ const updateScore = (req, res) => {
     Score.findOneAndUpdate({username: username}, {mail: mail, points: points, level: level})
         .then((data) => {
             res.send(data);
+        })
+}
+
+const registerPlayer = (req, res) => {
+    const {username, mail} = req.body;
+    const player = new Score({username: username, mail: mail});
+    player.save()
+        .then(() => {
+        res.send({status: "success"}
+    )});
+}
+
+const checkAvialability = (req, res) => {
+    Player.findOne({username: req.body.username})
+        .then((data) => {
+            if(data){
+                res.send({status: "0"})
+            }else{
+                res.send({status: "1"})
+            }
         })
 }
 
--- src/controllers/teamPage/team.page.controller.js
+++ src/controllers/teamPage/team.page.controller.js
@@ -2,7 +2,7 @@ const Score = require('../../models/player.model')
 
 const getTeam = (req, res) => {
     const adminName = req.User.adminName
-    Score.find({}).sort({rank: 1}).then((data) => {
+    Score.find({}).sort({level: 1}).then((data) => {
         res.render('admin/teamPage', {adminName, playerData : data })
     })
 }
