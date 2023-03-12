7a65debe3e0c7aa92748e5f135f6ab75a1682f9f
anuragc2001 <chakraborty.anurag01@gmail.com>
2023-03-12 13:40:35 +0530

    scoreboard sim added

--- src/services/cron.service.js
+++ src/services/cron.service.js
@@ -7,7 +7,7 @@ const dummyScoreboardSim = () => {
   const teams = ["pikachu", "aqua01", "mystik", "kaleido", "tessarus"]
   const random = Math.floor(Math.random() * teams.length);
   
-  axios.post('/update_score', {
+  axios.post(process.env.GAME_SCORE_UPDATE_URI, {
     username: teams[random],
     points: Math.floor(Math.random() * 100),
     level: Math.floor(Math.random() * 10),
